149c4786fb7fa7f0e0f21000a3c2312b06771f1d
Vitalii Orel <orelmsg@gmail.com>
2025-05-10 14:21:29 +0200

    Add settings

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Раздел дополнительных настроек</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
